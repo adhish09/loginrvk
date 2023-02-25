acc47082417eb63e79e14005e56b7d45e11ebae2
Adhish Raj <83546453+adhish09@users.noreply.github.com>
2023-02-25 16:39:18 +0530

    Add files via upload

---
+++ Signin.js
@@ -0,0 +1,28 @@
+import "./Signin.css";
+
+const Signin = () => {
+  return (
+    <div className="rectangle-parent">
+      <div className="frame-child" />
+      <div className="on-to-go">
+        <div className="start-your-journey">Login</div>
+      </div>
+      <div className="on-to-go1">
+        <div className="start-your-journey">Sign up</div>
+      </div>
+      <div className="on-to-go2">
+        <div className="start-your-journey">Skip to explore</div>
+      </div>
+      <div className="line-parent">
+        <div className="group-child" />
+        <div className="group-item" />
+        <div className="or">OR</div>
+      </div>
+      <div className="login-or-create">
+        Login or create an account to save your progress and see your stats
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
